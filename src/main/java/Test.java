c83e54806d43d2fbd4b8b0435320823ff7c67f96
zhsirhello <281797294@qq.com>
2019-03-18 20:34:23 +0800

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args){
+       /* HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("1",1);
+        hm.put("2",3);*/
+        /*Set<Map.Entry<String, Integer>> set = hm.entrySet();
+        Iterator<Map.Entry<String, Integer>> it = set.iterator();
+        while (it.hasNext()){
+            Map.Entry<String, Integer> next = it.next();
+            System.out.println(next.getKey()+"==="+next.getValue());
+        }*/
+    }
+}
