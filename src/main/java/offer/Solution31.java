052578cd16135affedc9df5bd9b3c41839f9286d
zhsirhello <281797294@qq.com>
2019-03-28 22:00:54 +0800

    二叉树的深度

---
+++ src/main/java/offer/Solution31.java
@@ -0,0 +1,44 @@
+package offer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 二叉树的深度
+ */
+public class Solution31 {
+    public int TreeDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public int TreeDepth1(TreeNode pRoot)
+    {
+        if(pRoot == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(pRoot);
+        int depth = 0, count = 0, nextCount = 1;
+        while(queue.size()!=0){
+            TreeNode top = queue.poll();
+            count++;
+            if(top.left != null){
+                queue.add(top.left);
+            }
+            if(top.right != null){
+                queue.add(top.right);
+            }
+            if(count == nextCount){
+                nextCount = queue.size();
+                count = 0;
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
