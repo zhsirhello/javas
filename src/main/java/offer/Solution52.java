3053821c0db8ffc77ee5340cc6916ed374959428
zhsirhello <281797294@qq.com>
2019-03-30 19:21:01 +0800

    序列化二叉树

---
+++ src/main/java/offer/Solution52.java
@@ -0,0 +1,34 @@
+package offer;
+
+/**
+ * 二叉树的序列化与反序列化
+ */
+public class Solution52 {
+    public int index = -1;
+    String Serialize(TreeNode root) {
+        StringBuffer sb = new StringBuffer();
+        if(root == null){
+            sb.append("#,");
+            return sb.toString();
+        }
+        sb.append(root.val + ",");
+        sb.append(Serialize(root.left));
+        sb.append(Serialize(root.right));
+        return sb.toString();
+    }
+    TreeNode Deserialize(String str) {
+        index++;
+        int len = str.length();
+        if(index >= len){
+            return null;
+        }
+        String[] strr = str.split(",");
+        TreeNode node = null;
+        if(!strr[index].equals("#")){
+            node = new TreeNode(Integer.valueOf(strr[index]));
+            node.left = Deserialize(str);
+            node.right = Deserialize(str);
+        }
+        return node;
+    }
+}
