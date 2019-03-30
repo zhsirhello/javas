132a7061f50d4ce060a73ef938cfa11b2921e1be
zhsirhello <281797294@qq.com>
2019-03-30 17:01:01 +0800

    删除链表中重复的结点

--- src/main/java/offer/Solution47.java
+++ src/main/java/offer/Solution47.java
@@ -5,7 +5,7 @@ package offer;
  * 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，
  * 重复的结点不保留，返回链表头指针。 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
  *
- * //暂未理解
+ * //只有在重复的时候next指针才需要起作用，所以有如下写法，有些难以理解，但注意是指针，指向即可。
  */
 public class Solution47 {
     public ListNode deleteDuplication(ListNode pHead) {
