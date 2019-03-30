f4061e7f70aa053ae4dda754de61c8114b41a60c
zhsirhello <281797294@qq.com>
2019-03-30 17:08:22 +0800

    yizhongshu

---
+++ src/main/java/offer/TreeLinkNode.java
@@ -0,0 +1,12 @@
+package offer;
+
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
