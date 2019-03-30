5a3dd151828667c16767357e5b29e42daa0edd1a
zhsirhello <281797294@qq.com>
2019-03-30 20:37:32 +0800

    滑动窗口的最大值

---
+++ src/main/java/offer/Solution55.java
@@ -0,0 +1,46 @@
+package offer;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+/**
+ * 滑动窗口的最大值
+ * 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。
+ * 例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，
+ * 他们的最大值分别为{4,4,6,6,6,5}； 针对数组{2,3,4,2,6,2,5,1}的滑动窗口有以下6个：
+ * {[2,3,4],2,6,2,5,1}， {2,[3,4,2],6,2,5,1}， {2,3,[4,2,6],2,5,1}， {2,3,4,[2,6,2],5,1}，
+ * {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
+ *
+ * 思路：用一个队列，头部永远存储的是未过期的最大的值
+ */
+public class Solution55 {
+    public static void main(String[] args){
+        int[] s={2,3,4,2,6,2,5,1};
+        ArrayList<Integer> list = new Solution55().maxInWindows(s, 3);
+        for(Integer i:list){
+            System.out.println(i);
+        }
+    }
+    public ArrayList<Integer> maxInWindows(int [] num, int size)
+    {
+
+        ArrayList<Integer> res = new ArrayList<>();
+        if(size == 0) return res;
+        int begin;
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for(int i = 0; i < num.length; i++){
+            begin = i - size + 1;
+            if(q.isEmpty())
+                q.add(i);
+            else if(begin > q.peekFirst())
+                q.pollFirst();
+
+            while((!q.isEmpty()) && num[q.peekLast()] <= num[i])
+                q.pollLast();
+            q.add(i);
+            if(begin >= 0)
+                res.add(num[q.peekFirst()]);
+        }
+        return res;
+    }
+}
