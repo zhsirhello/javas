2178cf459fcf6b8f6bafc416b95f9306a0e31c21
zhsirhello <281797294@qq.com>
2019-05-06 20:50:36 +0800

    关于java的swing与awt

---
+++ src/main/java/swingawt/FramTest.java
@@ -0,0 +1,11 @@
+package swingawt;
+
+import java.awt.*;
+
+public class FramTest {
+    public static void main(String args[]){
+        Frame f = new Frame("窗口");
+        f.setBounds(30,30,250,200);
+        f.setVisible(true);
+    }
+}
