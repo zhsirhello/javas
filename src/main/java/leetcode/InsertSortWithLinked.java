f8ad0b8d46d1a83b20306350dda533fcc4f7de98
zhsirhello <281797294@qq.com>
2019-05-22 15:09:52 +0800

    Sort a linked list using insertion sort.

---
+++ src/main/java/leetcode/InsertSortWithLinked.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * Sort a linked list using insertion sort.
+ */
+public class InsertSortWithLinked {
+    public ListNode insertionSortList(ListNode head) {
+        //哑节点
+        ListNode dumy = new ListNode(Integer.MIN_VALUE);
+        ListNode cur = head;
+        ListNode pre = dumy;
+        while (cur != null) {
+            //保存当前节点下一个节点
+            ListNode next = cur.next;
+            pre = dumy;
+            //寻找当前节点正确位置的一个节点
+            while (pre.next != null && pre.next.val < cur.val) {
+                pre = pre.next;
+            }
+            //将当前节点加入新链表中
+            cur.next = pre.next;
+            pre.next = cur;
+            //处理下一个节点
+            cur = next;
+        }
+        return dumy.next;
+
+    }
+}
