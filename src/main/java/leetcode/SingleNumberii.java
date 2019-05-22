df8eb37fd47b582aaa0b141fb6801f4d4ad4fff6
zhsirhello <281797294@qq.com>
2019-05-22 17:42:39 +0800

    不懂

---
+++ src/main/java/leetcode/SingleNumberii.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * 给定一个整数数组，除了一个元素外，每个元素都会出现三次。 找一个单一的。
+ */
+public class SingleNumberii {
+    public int singleNumber(int[] A) {
+        int a = 0 , b = 0;
+        for(int c : A){
+            int ta,tb;
+            ta = a;
+            tb = b;
+            a = (ta&(~tb)&(~c))|((~ta)&tb&c);
+            b = ~ta&((~c&tb)|(~tb&c));
+        }
+        return a|b;
+    }
+}
