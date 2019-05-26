f3b4543636157bc0ee07666c7043e99181ede231
zhsirhello <281797294@qq.com>
2019-05-26 22:06:23 +0800

    合并两个有序数组，nums1有足够的空间，不许自己新建立空间

---
+++ src/main/java/leetcode/MergeSortedArray.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * 合并两个有序数组，nums1有足够的空间，不许自己新建立空间
+ */
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, index = m + n - 1;
+        while (i >= 0 && j >= 0)
+            nums1[index--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        while (j >= 0)
+            nums1[index--] = nums2[j--];
+    }
+}
