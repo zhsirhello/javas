b08bedd3dd0666bc29b0b25a3a1c7b765f441fd1
zhsirhello <281797294@qq.com>
2019-05-29 16:28:25 +0800

    数组中的元素表示它可以跳跃几个索引，判断从头开始，是否能跳到最后

--- src/main/java/leetcode/JumpGame.java
+++ src/main/java/leetcode/JumpGame.java
@@ -14,6 +14,7 @@ public class JumpGame {
     public boolean canJump(int[] A) {
         int maxReach = 0;
         int n=A.length;
+        //精华部分。maxReach表示当前在i处当前可达到的最大索引，如果i>maxReach，说明当前不能再走了，因为走不动了。
         for(int i=0;i<n && i<=maxReach;i++)
             maxReach = Math.max(maxReach,i+A[i]);  /// 跳到该点后还能到达的极限
         if(maxReach<n-1)
