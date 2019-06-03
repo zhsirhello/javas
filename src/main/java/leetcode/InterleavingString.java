bec79a7d0f35debbbe2d6ec6789f693fb7092c94
zhsirhello <281797294@qq.com>
2019-06-03 21:37:55 +0800

    判断s1与s2是否能组成s3

--- src/main/java/leetcode/InterleavingString.java
+++ src/main/java/leetcode/InterleavingString.java
@@ -1,40 +1,55 @@
 package leetcode;
 
+/**
+ * 判断s1与s2是否能组成s3
+ * 动态规划
+ * dp[i][j]代表 chs1[0...i-1]  chs2[0...j-1]能否顺序匹配chs3[i+j-1]
+ *
+ * Given:
+ * s1 ="aabcc",
+ * s2 ="dbbca",
+ *
+ * When s3 ="aadbbcbcac", return true.
+ * When s3 ="aadbbbaccc", return false.
+ */
 public class InterleavingString {
     public static void main(String[] args){
         System.out.println(new InterleavingString().isInterleave("a","b","ab"));
     }
     public boolean isInterleave(String s1, String s2, String s3) {
-        String temp=s1;
-        s1=s3;
-        s3=temp;
-        if((s1.equals("")||s1==null)&&(s2.equals("")||s2==null)&&(s3.equals("")||s3==null)){
-            return true;
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+
+        if(len1+len2 !=len3){
+            return false;
         }
-        if(s2.equals("")||s2==null){
-            return s1.equals(s3);
+
+        char[] chs1 = s1.toCharArray();
+        char[] chs2 = s2.toCharArray();
+        char[] chs3 = s3.toCharArray();
+
+        //dp[i][j]代表 chs1[0...i-1]  chs2[0...j-1]能否顺序匹配chs3[i+j-1]
+        boolean[][] dp = new boolean[len1+1][len2+1];
+        //初始化 s1中取0个字符 s2中取0个字符 匹配s3从0开始的0个字符 肯定匹配true
+        dp[0][0] = true;
+
+        //s1中取0个s2中取i个 去和s3中0+i 个匹配
+        for(int i = 1 ; i < len2 + 1; i ++ ){
+            dp[0][i] = dp[0][i-1] && chs2[i-1] == chs3[i-1];
         }
-        if(s3.equals("")||s3==null){
-            return s1.equals(s2);
+        //s2中取0个s1中取i个 去和s3中0+i 个匹配
+        for(int i = 1 ; i < len1 + 1; i ++ ){
+            dp[i][0] = dp[i-1][0] && chs1[i-1] == chs3[i-1];
         }
-        if(s1.charAt(0)==s2.charAt(0)&&s1.charAt(0)==s3.charAt(0)){
-            if(s2.length()==1&&s3.length()==1){
-                return isInterleave(s1.substring(1),"",s3)&&isInterleave(s1.substring(1),s2,"");
-            }else if(s2.length()==1&&s3.length()!=1){
-                return isInterleave(s1.substring(1),"",s3)&&isInterleave(s1.substring(1),s2,s3.substring(1));
-            }else if(s2.length()!=1&&s3.length()==1){
-                return isInterleave(s1.substring(1),s2.substring(1),s3)&&isInterleave(s1.substring(1),s2,"");
-            }else  return isInterleave(s1.substring(1),s2.substring(1),s3)&&isInterleave(s1.substring(1),s2,s3.substring(1));
-        }else if(s1.charAt(0)!=s2.charAt(0)&&s1.charAt(0)==s3.charAt(0)){
-            if(s3.length()==1){
-                return isInterleave(s1.substring(1),s2,"");
-            }else return isInterleave(s1.substring(1),s2,s3.substring(1));
-        }else if(s1.charAt(0)==s2.charAt(0)&&s1.charAt(0)!=s3.charAt(0)){
-            if(s2.length()==1){
-                return isInterleave(s1.substring(1),"",s3);
-            }else {
-                return isInterleave(s1.substring(1),s2.substring(1),s3);
+
+        for(int i = 1 ; i < len1+1 ; i ++ ){
+            for(int j = 1 ; j < len2+1 ; j ++ ){
+                dp[i][j] = dp[i-1][j] && (chs3[i+j-1] == chs1[i-1])
+                        || dp[i][j-1] && (chs3[i+j-1] == chs2[j-1]);
             }
-        }else return false;
+        }
+
+        return dp[len1][len2];
     }
 }
