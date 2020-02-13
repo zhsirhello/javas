4725a3d7c2ecca0b0e6a0bf2a819bc1fdf2f084b
zhsirhello <281797294@qq.com>
2020-02-13 15:29:42 +0800

    protobuf

---
+++ src/main/java/netty/protobuf/SubReqServer.java
@@ -0,0 +1,61 @@
+package netty.protobuf;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * netty使用Google Protobuf编解码
+ * 1、编解码的类需要使用Protobuf工具生成，注意，这些生成的类有诸如toByteArray将对象转换成byte数组，parseFrom将byte数组解码为原始对象，等方法。
+ * 2、pipeline.addlast那儿需要添加，并且要加半包处理的参数，否则运行错误
+ */
+public class SubReqServer {
+
+    public void bind(int port) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try{
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //主要用于半包处理
+                            ch.pipeline().addLast(new ProtobufVarint32FrameDecoder());
+                            //解码目标类
+                            ch.pipeline().addLast(new ProtobufDecoder(SubscribeReqProto.SubscribeReq.getDefaultInstance()));
+                            ch.pipeline().addLast(new ProtobufVarint32LengthFieldPrepender());
+                            ch.pipeline().addLast(new ProtobufEncoder());
+                            ch.pipeline().addLast(new SubReqServerHandler());
+
+                        }
+                    });
+            ChannelFuture f = serverBootstrap.bind(port).sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+    public static void main(String[] args) throws InterruptedException {
+        new SubReqServer().bind(8080);
+    }
+}
